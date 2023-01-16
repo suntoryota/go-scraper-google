e23afadfdb6eb4e0eda06f53b7c26aaa58ecf0a0
suntoryota <ryotasunto@gmail.com>
2023-01-16 19:06:21 +0700

    go-scraper-google

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type SearchResult struct {
+	ResultRank  int
+	ResultURL   string
+	ResultTitle string
+	ResultDes   string
+}
+
+var googleDomains = map[string]string{
+	"com": "https://www.google.com/search?q=",
+	"jp":  "https://www.google.co.jp/search?q=",
+}
+
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+}
+
+func randomUserAgent() string {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Int() % len(userAgents)
+	return userAgents[randNum]
+}
+
+func buildGoogleUrls(searchTerm, countryCode, languageCode string, pages, count int) ([]string, error) {
+	toScrape := []string{}
+	searchTerm = strings.Trim(searchTerm, " ")
+	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
+	if googleBase, found := googleDomains[countryCode]; found {
+		for i := 0; i < pages; i++ {
+			start := i * count
+			scrapeURL := fmt.Sprintf("%s%s&num=%d&hl=%s&start=%d&filter=0", googleBase, searchTerm, count, languageCode, start)
+			toScrape = append(toScrape, scrapeURL)
+		}
+	} else {
+		err := fmt.Errorf("country (%s) is currently not supported", countryCode)
+		return nil, err
+	}
+	return toScrape, nil
+}
+
+func getScrapeClient(proxyString interface{}) *http.Client {
+	switch v := proxyString.(type) {
+
+	case string:
+		proxyUrl, _ := url.Parse(v)
+		return &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	default:
+		return &http.Client{}
+	}
+}
+
+func googleResultParsing(response *http.Response, rank int) ([]SearchResult, error) {
+	doc, err := goquery.NewDocumentFromResponse(response)
+	if err != nil {
+		return nil, err
+	}
+
+	results := []SearchResult{}
+	sel := doc.Find("div.g")
+	rank++
+	for i := range sel.Nodes {
+		item := sel.Eq(i)
+		linkTag := item.Find("a")
+		link, _ := linkTag.Attr("href")
+		titleTag := item.Find("h3.r")
+		descTag := item.Find("span.st")
+		desc := descTag.Text()
+		title := titleTag.Text()
+		link = strings.Trim(link, " ")
+
+		if link != "" && link != "#" && !strings.HasPrefix(link, "/") {
+			result := SearchResult{
+				rank,
+				link,
+				title,
+				desc,
+			}
+			results = append(results, result)
+			rank++
+		}
+	}
+	return results, err
+}
+
+func GoogleScrape(searchTerm, countryCode, languageCode string, proxyString interface{}, pages, count int, backoff int) ([]SearchResult, error) {
+	results := []SearchResult{}
+	resultCounter := 0
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode, languageCode, pages, count)
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range googlePages {
+		res, err := scrapeClientRequest(page, proxyString)
+		if err != nil {
+			return nil, err
+		}
+		data, err := googleResultParsing(res, resultCounter)
+		if err != nil {
+			return nil, err
+		}
+		resultCounter += len(data)
+		for _, result := range data {
+			results = append(results, result)
+		}
+		time.Sleep(time.Duration(backoff) * time.Second)
+	}
+	return results, nil
+}
+
+func scrapeClientRequest(searchURL string, proxyString interface{}) (*http.Response, error) {
+	baseClient := getScrapeClient(proxyString)
+	req, _ := http.NewRequest("GET", searchURL, nil)
+	req.Header.Set("User-Agent", randomUserAgent())
+
+	res, err := baseClient.Do(req)
+	if res.StatusCode != 200 {
+		err := fmt.Errorf("scraper received a non-200 status code suggesting a ban")
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func main() {
+	res, err := GoogleScrape("sunto ryota", "com", "en", nil, 1, 30, 10)
+	if err == nil {
+		for _, res := range res {
+			fmt.Println(res)
+		}
+	}
+}
